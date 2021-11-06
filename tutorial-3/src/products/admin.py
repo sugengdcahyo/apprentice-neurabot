a2725a2288c1514cda16b0495a076eb28640e814
Sugeng D. Cahyo <sugengdcahyo@gmail.com>
2021-11-06 10:30:22 +0700

    insomnia docs

--- tutorial-3/src/products/admin.py
+++ tutorial-3/src/products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from products.models import Product
 
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'type', 'created_at', 'updated_at')
